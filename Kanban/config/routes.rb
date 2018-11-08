62a38956c74a1d8182888e315c021d4bd3bdea00
David Miller <dmill146@uncc.edu>
2018-11-08 23:09:42 +0000

    fixed board, comment and task generator

--- Kanban/config/routes.rb
+++ Kanban/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :boards
+  resources :tasks
+  resources :comments
   get 'board/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- Kanban/db/migrate/20181030233457_create_comments.rb
+++ Kanban/db/migrate/20181030233457_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.integer :id
       t.text :body
       t.string :title
 
