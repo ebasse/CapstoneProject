8fc6c241424e771d90568b38b69b2d8c22b0796a
Sylvester Mensah <smensah1@uncc.edu>
2018-11-30 08:41:06 +0000

    Add search action for members and error messages

--- Kanban/app/controllers/users_controller.rb
+++ Kanban/app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
     def my_members
         @memberships = current_user.members
     end
+    
+    def search
+        if params[:search_param].blank?
+            flash.now[:danger] = "Oops! Enter users name or email"
+        else
+            @users = User.search(params[:search_param])
+            flash.now[:danger] = "No users match this search criteria" if @users.blank?
+        end
+        respond_to do |format|
+            format.js { render partial: 'members/result' }
+        end
+    end
 end
--- Kanban/config/routes.rb
+++ Kanban/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  
   root 'welcome#index'
+  resources :users, only: [:show] #place holder to show board maybe
+  resources :memberships
   get 'my_members', to: 'users#my_members'
+  get 'search_members', to: 'users#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
